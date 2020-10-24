d910e2c82a4bc199fe95d24f0dca37b38c2923c8
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2020-10-24 14:00:34 +0900

    jsをシェアに対応

--- public/js/liff.js
+++ public/js/liff.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
       liffId: '1654949077-Mxbj67nj'
     })
     .then(() => {
-      document.getElementById('go').addEventListener('click', () => {
+      document.getElementById('talk').addEventListener('click', () => {
         if (!liff.isInClient()) {
           document.getElementById('log').value += '[ERROR!]作成失敗！\n';
         } else {
@@ -46,6 +46,53 @@ document.addEventListener("DOMContentLoaded", () => {
           },{
             'type': 'text',
             'text': 'おねがいをコピーして使ってね！'
+          }]).then(function() {
+            liff.closeWindow();
+          }).catch(function(error) {
+            document.getElementById('log').value += '[ERROR!]sendMessagesText()=' + error + '\n';
+          });
+        }
+      });
+
+      document.getElementById('share').addEventListener('click', () => {
+        if (!liff.isInClient()) {
+          document.getElementById('log').value += '[ERROR!]作成失敗！\n';
+        } else {
+          let sendMessages ="";
+          sendMessages = "【"+getRQ('rqType')+"クエスト】\n";
+          sendMessages += "『"+getRQ('rqName')+"』\n";
+
+          let rateFlag = false;
+          let rate = getRQ('rqRate');
+          if (!isNaN(Number(rate))) {
+            if (Number.isInteger(Number(rate))) {
+              rate = parseInt(rate, 10);
+              if (rate > 8) {
+                rate = 8;
+              } else if (rate < 0) {
+                rate = 0;
+              }
+              const blackStar = '★';
+              const whiteStar = '☆';
+              sendMessages += blackStar.repeat(rate);
+              sendMessages += whiteStar.repeat(8-rate);
+              sendMessages += "\n";
+
+              rateFlag = true;
+            }
+          }
+          if (!rateFlag) {
+            sendMessages += "[難易度]\n"+rate+"\n";
+          }
+
+          sendMessages += "[目標]\n"+getRQ('rqTarget')+"\n";
+          sendMessages += "[報酬]\n"+getRQ('rqReward')+"\n";
+          sendMessages += "[制限時間]\n"+getRQ('rqTimelimit')+"\n------------\n";
+          sendMessages += "[依頼主のコメント]\n"+getRQ('rqComment');
+
+          liff.sendMessages([{
+            'type': 'text',
+            'text': sendMessages
           }]).then(function() {
             liff.closeWindow();
           }).catch(function(error) {
